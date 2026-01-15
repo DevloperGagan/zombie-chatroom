3e6c5c1b4ecbac482315f2f081ad947145c243bf
Himanshu Pradhan <64770860+DevloperGagan@users.noreply.github.com>
2026-01-15 22:17:03 +0530

    Fix databaseURL format in firebaseConfig
    Removed trailing slash from databaseURL in firebaseConfig.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import './App.css';
 const firebaseConfig = {
   apiKey: "AIzaSyCC3ENCbXGHnGGDLkFg3SY5545wobIiz64",
   authDomain: "zombie-64df0.firebaseapp.com",
-  databaseURL: "https://zombie-64df0-default-rtdb.firebaseio.com/",
+  databaseURL: "https://zombie-64df0-default-rtdb.firebaseio.com",
   projectId: "zombie-64df0",
   storageBucket: "zombie-64df0.appspot.com",
   messagingSenderId: "388952215569",
-  appId: "1:388952215569:web:cf8954d1628c3dce2961b0",
-  measurementId: "G-MWJFFQ3LZF"
+  appId: "1:388952215569:web:cf8954d1628c3dce2961b0"
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
 
 function App() {
   const [username, setUsername] = useState('');
