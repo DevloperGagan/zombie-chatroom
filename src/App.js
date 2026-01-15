6f6032b83ba96b3fc543638da4061600f8014654
Himanshu Pradhan <64770860+DevloperGagan@users.noreply.github.com>
2026-01-15 18:39:08 +0530

    Refactor Firebase setup in App.js
    Updated Firebase configuration and initialization logic.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import firebase from 'firebase/compat/app'; // import syntax for Firebase
-import 'firebase/compat/database'; //import syntax for Firebase Realtime Database
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 import WelcomePopup from './WelcomePopup';
 import ChatRoom from './ChatRoom';
 import './App.css';
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCC3ENCbXGHnGGDLkFg3SY5545wobIiz64",
   authDomain: "zombie-64df0.firebaseapp.com",
+  databaseURL: "https://zombie-64df0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "zombie-64df0",
-  storageBucket: "zombie-64df0.firebasestorage.app",
+  storageBucket: "zombie-64df0.appspot.com",
   messagingSenderId: "388952215569",
   appId: "1:388952215569:web:cf8954d1628c3dce2961b0",
   measurementId: "G-MWJFFQ3LZF"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
   const [username, setUsername] = useState('');
